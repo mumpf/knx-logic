22e47c8c17fbf739db37e71f2172b0e874797a62
Waldemar Porscha <waldemar.porscha@sap.com>
2020-04-07 18:42:59 +0200

    - added WIREGATEWAY to support logic there - new channels are now "unbenannt", not "undefiniert"

--- src/LogicChannel.h
+++ src/LogicChannel.h
@@ -17,6 +17,9 @@
 #elif PMMODULE
 #define MODULE "PMMODULE"
 #include "../../knx-pm/src/PMmodul.h"
+#elif WIREGATEWAY
+#define MODULE "WIREGATEWAY"
+#include "../../knx-wire/src/WireGateway.h"
 #elif TEST
 #include "../../knx-test/src/Test.h"
 #define MODULE "TEST"
